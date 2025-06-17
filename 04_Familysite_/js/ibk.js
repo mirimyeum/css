b41f68308d3e04806dd9c35dc444d02c5f868d6f
mirimyeum <mirimyeum83@naver.com>
2025-06-17 17:27:50 +0900

    Add files via upload

---
+++ 04_Familysite_/js/ibk.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    $(".family2 button").click(function(){
+        $(".list2").toggle();
+        $(".list1").hide();
+    })
+
+    $(".family1 button").click(function(){
+        $(".list1").toggle();
+        $(".list2").hide();
+
+    })
+
+
+
+
+})
