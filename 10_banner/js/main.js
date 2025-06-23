12fd027acbcf1126451c016f9798509918822178
mirimyeum <mirimyeum83@naver.com>
2025-06-23 16:25:06 +0900

    Add files via upload

---
+++ 10_banner/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.list').slick({
+        // 자동실행
+    autoplay: true,
+    // 한화면에 보일 슬라이드 갯수 설정
+    slidesToShow:2,
+    slidesToScroll:1,
+    // 좌우버튼 숨기기
+    arrows: false,
+    // 페이지 버튼 보이기
+    dots: true,
+  });
+})
