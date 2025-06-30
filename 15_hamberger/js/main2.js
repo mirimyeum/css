5ee3e80e90075b49a85ee8152f89d7577a6c5343
mirimyeum <mirimyeum83@naver.com>
2025-06-30 15:32:23 +0900

    Add files via upload

---
+++ 15_hamberger/js/main2.js
@@ -0,0 +1,13 @@
+// 제이쿼리 실행전 준비작업(바인딩)
+$(document).ready(function(){
+    // 클릭이벤트
+    $(".box a").click(function(){
+        $(".box a").toggleClass("on");
+    })
+
+
+})
+
+// $(function(){
+
+// })
