7e563f088c231a0a805637fc0b4bcef3e4a6c43f
mirimyeum <mirimyeum83@naver.com>
2025-07-10 17:28:16 +0900

    Add files via upload

--- 08_mobileslide(subway)/js/main.js
+++ 08_mobileslide(subway)/js/main.js
@@ -27,4 +27,32 @@ $(function(){
   },
 });
 
+// 배너슬라이드 slide슬라이드
+  $('.banner_slide').slick({
+    autoplay:true,
+    slidesToShow:2,
+    // 좌우버튼 숨기기
+    arrows:false,
+    // 페이지 버튼 보이기
+    dots: true
+  });
+
+  $(".m_gnb>ul>li>a").click(function(){
+    $(this).next().slideToggle().parent().siblings().find(".depth2").slideUp();
+  });
+
+  // 햄버거 버튼 클릭시 m_side_wrap을 left -100%에서 0으로 튀어나오게하기
+  $(".m_btn").click(function(){
+    $(".cover").fadeIn()
+    $(".m_side_wrap").animate({"left":0},300);
+  })
+
+  // 닫기버튼 클릭시 브라우저 바깥으로 보내기
+  $(".m_close").click(function(){
+    $(".m_side_wrap").animate({"left":'-100%'},300);
+    $(".cover").fadeOut()
+  })
+
+  // 메뉴 클릭에만 커버띄우기
+  
 })
