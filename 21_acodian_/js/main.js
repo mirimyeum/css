ee6d17202ae95dc6e3292601ed08660d2101a077
mirimyeum <mirimyeum83@naver.com>
2025-07-10 11:28:22 +0900

    Add files via upload

---
+++ 21_acodian_/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    // 주메뉴 클릭시 2단메뉴 열리게
+    $(".box>ul>li>a").click(function(){
+        $(this).next().slideToggle().parent().siblings().find(".depth2").slideUp();
+    })
+})
+
+// $(document).ready({
+
+// })
